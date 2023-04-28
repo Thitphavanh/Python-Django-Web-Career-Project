a447d5c41099d57f51485baada38df4bc092aa96
Thitphavanh <thitphavanh@gmail.com>
2023-04-28 15:25:24 +0700

    updated

--- web_career_project/web_career_project/urls.py
+++ web_career_project/web_career_project/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("myapp.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='webcareer/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='webcareer/logout.html'), name='logout'),
 ]
