7d0f4eaa6f7c0b803d8d5d414e52b09c74ff3c09
Thitphavanh <thitphavanh@gmail.com>
2023-05-05 00:43:18 +0700

    updated

--- web_career_project/web_career_project/urls.py
+++ web_career_project/web_career_project/urls.py
@@ -11,3 +11,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='webcareer/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='webcareer/logout.html'), name='logout'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
